027bea391966c299ec9e0d28138a091646dff9a8
arfycat <arfycat@users.noreply.github.com>
2022-07-16 16:05:36 -0400

    Changed some paths to reflect my installed config, removed some extraneous files & lines.

--- config/dnscrypt-proxy/dnscrypt-watchdog.sh
+++ config/dnscrypt-proxy/dnscrypt-watchdog.sh
@@ -14,18 +14,18 @@ while true; do
     if pgrep -F /var/run/dnscrypt-proxy.pid > /dev/null
     then
 #        /usr/bin/logger -p user.notice dnscrypt-watchdog: dnscrypt-proxy process is OK, checking resolver...
-        host 9.9.9.9 > /dev/null
+        host 1.1.1.1 > /dev/null
         if [ $? -ne 0 ]
         then
             fail_count=$((fail_count + 1))
             /usr/bin/logger -p user.notice DNS check to www.quad9.net FAIL, count: $fail_count
 
             if [ $fail_count -ge ${RESET_WHEN_FAIL_COUNT} ]
-	        then
+                then
                 /usr/bin/logger -p user.notice Stopping dnscrypt-proxy...
-                /config/sbin/dnscrypt-proxy/dnscrypt-proxy -service stop
+                /config/dnscrypt-proxy/dnscrypt-proxy -service stop
                 /usr/bin/logger -p user.notice Starting dnscrypt-proxy...
-                /config/sbin/dnscrypt-proxy/dnscrypt-proxy -service start
+                /config/dnscrypt-proxy/dnscrypt-proxy -service start
 
                 /usr/bin/logger -p user.notice Sending SIGHUP to dnsmasq...
                 /usr/bin/pkill -SIGHUP -e -F /var/run/dnsmasq/dnsmasq.pid 2>&1 | logger -p user.notice
@@ -38,7 +38,7 @@ while true; do
         fi
     else
         /usr/bin/logger -p user.notice dnscrypt-watchdog WARNING: dnscrypt-proxy process DOA, restarting...
-        /config/sbin/dnscrypt-proxy/dnscrypt-proxy -service start
+        /config/dnscrypt-proxy/dnscrypt-proxy -service start
 
         /usr/bin/logger -p user.notice dnscrypt-watchdog Sending SIGHUP to dnsmasq...
         /usr/bin/pkill -SIGHUP -e -F /var/run/dnsmasq/dnsmasq.pid 2>&1 | logger -p user.notice
--- config/scripts/post-config.d/dnscrypt.sh
+++ config/scripts/post-config.d/dnscrypt.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Check that persistent (/config filesystem) var directory exists, if not, create it and set ownership
-if [ ! -d /config/var/run/dnsmasq ]; then
-    /bin/mkdir -p /config/var/run/dnsmasq;
-    /bin/chown dnsmasq /config/var/run/dnsmasq;
-fi;
-
-/config/sbin/dnscrypt-proxy/dnscrypt-proxy -service install
-/config/sbin/dnscrypt-proxy/dnscrypt-proxy -service start
+/config/dnscrypt-proxy/dnscrypt-proxy -service install
+/config/dnscrypt-proxy/dnscrypt-proxy -service start
 
 # Start watchdog
-#(/config/sbin/dnscrypt-proxy/dnscrypt-watchdog.sh) &
+#(/config/dnscrypt-proxy/dnscrypt-watchdog.sh) &
 
 exit 0
